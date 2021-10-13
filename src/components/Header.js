6f97d1e04d062320ca1e84a6e4c25e6f9c19b639
Stackbit Code Editor <projects@stackbit.com>
2021-10-13 16:45:21 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ export default class Header extends React.Component {
                                 </svg>
                             </button>
                             <div className="hamburger__content"{...(_.get(site, 'data.config.hamburger_background_image', null) ? ({style: toStyleObj('background-image: url(\'' + withPrefix(_.get(site, 'data.config.hamburger_background_image', null)) + '\')')}) : null)}>
-                                <div className="hamburger__options">
+                                <!-- <div className="hamburger__options">
                                     <button className="snipcart-checkout button button--transparent">
                                         <span className="button__icon nav__button-icon">
                                             <svg width="24" height="25" viewBox="0 0 24 25" xmlns="http://www.w3.org/2000/svg">
@@ -71,7 +71,7 @@ export default class Header extends React.Component {
                                     <svg width="26" height="14" viewBox="0 0 26 14" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M22.682 6.079h-22.682v1.712h22.814l-4.574 4.528 1.194 1.182 6.566-6.5-6.566-6.5-1.194 1.182 4.442 4.397z" />
                                     </svg>
-                                </button>
+                                </button> -->
                                 <ul className="hamburger__nav">
                                     {_.map(_.get(site, 'data.config.main_menu', null), (item, item_idx) => {
                                         let section = _.get(page, 'frontmatter.section', null) || _.get(page, 'frontmatter.title', null);
