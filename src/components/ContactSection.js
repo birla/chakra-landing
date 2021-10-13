c52384df44467ea75034423c37d45d3fa06038eb
Stackbit Code Editor <projects@stackbit.com>
2021-10-13 19:02:07 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -15,7 +15,7 @@ export default class ContactSection extends React.Component {
                   <p className="contact__address">{_.get(section, 'address', null)}</p>
                   <p className="contact__telephone">{_.get(section, 'phone', null)}</p>
                   <p className="contact__email">
-                      <Link href={_.get(section, 'email', null)}>
+                      <Link href={"mailto:" + _.get(section, 'email', null)}>
                           {_.get(section, 'email', null)}
                       </Link>
                   </p>
