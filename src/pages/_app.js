f1caa568103fb406163a6eac69dbb59827be6862
Stackbit Code Editor <projects@stackbit.com>
2021-10-14 06:58:20 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@
 import { useEffect } from 'react';
 import Router from 'next/router';
 import '../sass/main.scss';
-import * as ga from '../lib/ga'
+import * as ga from '../lib/ga';
 
 function MyApp({ Component, pageProps }) {
 
