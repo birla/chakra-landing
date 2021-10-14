47f80a4dd8103554cf74ba301a005c8c2b847820
Stackbit Code Editor <projects@stackbit.com>
2021-10-14 07:35:25 +0000

    ga.js: changed

--- src/lib/ga.js
+++ src/lib/ga.js
@@ -2,7 +2,7 @@ const gaKey = require('../../content/data/config.json').google_analytics_key;
 
 // log the pageview with their URL
 export const pageview = (url) => {
-  if (if (window.onNextjsRouteChangeComplete) {)
+  if (window.onNextjsRouteChangeComplete) {
   window.gtag('config', gaKey, {
     page_path: url,
   })
